d26f707dc39554ea6e4fbcf23a801a78ee910c59
halljosh <joshuaandrewhall@gmail.com>
2020-12-07 11:40:41 -0500

    added header access

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(bodyParser.json()); // parses JSON data from incoming requests
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // allows any origin to access the server
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // allows these methods to be used by the client
+    res.setHeader('Access-Control-Allow-Headers', '*'); // allows client to set any headers
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
